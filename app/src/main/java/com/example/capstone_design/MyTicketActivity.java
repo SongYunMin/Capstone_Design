38d1bfb0dc11b895400d841e147cd340522a4c0c
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-02-29 15:50:17 +0900

    Ticket Button Event 처리 완료

--- app/src/main/java/com/example/capstone_design/MyTicketActivity.java
+++ app/src/main/java/com/example/capstone_design/MyTicketActivity.java
@@ -1,6 +1,8 @@
 package com.example.capstone_design;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
+
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,7 +12,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
-
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 import static com.example.capstone_design.NewAccount_Activity.ReservationWhether;
 public class MyTicketActivity extends AppCompatActivity {
@@ -29,7 +30,6 @@ public class MyTicketActivity extends AppCompatActivity {
         final Button TicketButton = (Button)findViewById(R.id.my_ticket_button);
         final TextView NoTicketText = (TextView)findViewById(R.id.noticket);
 
-
          // MyTIcket Activity (XML)
           if(ReservationWhether.equals("1")){
             TicketImage.setImageResource(R.drawable.bts);
@@ -37,32 +37,52 @@ public class MyTicketActivity extends AppCompatActivity {
             DayText.setText("2019.10.26");
             TimeText.setText("18:00");
             PlaceText.setText("OLIMPIC STADIUM");
-        }
+            TicketButton.setVisibility(View.VISIBLE);
+
+          }
         else if(ReservationWhether.equals("2")){
             TicketImage.setImageResource(R.drawable.kkh);
             NameText.setText("KimKyungHo Concert");
             DayText.setText("2019.10.26");
             TimeText.setText("19:30");
             PlaceText.setText("Chunchepn Art Center");
-        }
+            TicketButton.setVisibility(View.VISIBLE);
+          }
         else if(ReservationWhether.equals("3")){
             TicketImage.setImageResource(R.drawable.mmmia);
             NameText.setText("MAMMAMIA!");
             DayText.setText("2019.10.29");
             TimeText.setText("18:00");
             PlaceText.setText("Seoul Art Center");
-        }
+            TicketButton.setVisibility(View.VISIBLE);
+
+          }
         else if(ReservationWhether.equals("4")){
             TicketImage.setImageResource(R.drawable.skj);
             NameText.setText("Seo Kang Jun Fan Concert");
             DayText.setText("2020.1.4");
             TimeText.setText("14:00");
             PlaceText.setText("Suwon WorldCup Stadium");
-        }
+            TicketButton.setVisibility(View.VISIBLE);
+
+          }
         else{
             NoTicketText.setText("예약된 티켓이 없습니다");
             NoTicketText.setVisibility(View.VISIBLE);
         }
+
+        // Ticket Button에 대한 Click Event 처리
+        TicketButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(ReservationWhether != "0"){
+                    Intent intent = new Intent(getApplicationContext(), NFCActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+
+        });
 
     }
 }
