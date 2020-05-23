8979c76f909473a5fdaa0affecb98854887359f6
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-05-23 19:15:18 +0900

    로그인 정보 기록 Class init

---
+++ app/src/main/java/com/example/capstone_design/SharedPreference.java
@@ -0,0 +1,13 @@
+package com.example.capstone_design;
+
+import android.content.Context;
+
+import static com.example.capstone_design.LoginActivity.St_id;
+
+public class SharedPreference {
+    static final String PREF_USER_NAME = St_id;
+
+//    static SharedPreference getSharedPreferences(Context ctx){
+//
+//    }
+}
