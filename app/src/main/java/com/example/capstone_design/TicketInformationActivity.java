e9406d3207416adf750e6607a7147405341a4a58
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-06-10 14:22:22 +0900

    Ticket_information Activity 생성

---
+++ app/src/main/java/com/example/capstone_design/TicketInformationActivity.java
@@ -0,0 +1,16 @@
+package com.example.capstone_design;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import static com.example.capstone_design.NewAccount_Activity.ReservationWhether;
+import static com.example.capstone_design.NewAccount_Activity.status;
+public class TicketInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_information);
+
+        
+    }
+}
